329006b4a4db30ef9d8d8b4cf35caf2eca99d696
paul-andrei-pangan <paulandreipangan09@gmail.com>
2025-03-21 20:50:49 +0800

    Restore widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:taskmanager/main.dart';
+import 'package:taskmanager/main.dart'; // Siguraduhin na tama ang import path
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build the app
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Hanapin ang + button
+    final Finder fab = find.byIcon(Icons.add);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Siguraduhin na may isa lang
+    expect(fab, findsOneWidget);
+
+    // I-tap ang button
+    await tester.tap(fab);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    // Hanapin kung nag-increment ang counter
     expect(find.text('1'), findsOneWidget);
   });
 }
